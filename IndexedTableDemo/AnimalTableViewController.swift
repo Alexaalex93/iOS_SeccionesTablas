611cb7028ec04f79d3acf977c366e055f55ddbe9
cice <cice@aula1PovMacN09.local>
2017-02-15 09:54:06 +0100

    INDICE LATERAL

--- IndexedTableDemo/AnimalTableViewController.swift
+++ IndexedTableDemo/AnimalTableViewController.swift
@@ -13,6 +13,8 @@ class AnimalTableViewController: UITableViewController {
     
     let animales: [String:[String]] = ["B":["Bear", "Black Swan", "Bufalo"]]
     
+    let animalSectionTitles = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    
     var animalesDict = [String:[String]] ()
     var animalesTituloSeccion = [String]()
     
@@ -24,8 +26,21 @@ class AnimalTableViewController: UITableViewController {
         
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        crearDiccAnimales()
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        crearDiccAnimales()
+    }
+    
+    
+    
+    
+    
+    
+    //CREA LOS DICCIONARIOS CON LOS ANIMALES Y SUS CLAVES
     func crearDiccAnimales() {
     
         for animal in animals {
@@ -61,11 +76,7 @@ class AnimalTableViewController: UITableViewController {
     
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-        crearDiccAnimales()
-    }
+
     
     
     
@@ -80,8 +91,41 @@ class AnimalTableViewController: UITableViewController {
         return animalesTituloSeccion[section]
     }
     
+    //CREA UNA BARRA LATERAL CON LETRAS
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? { //Esta funcion sirve para poner la barra lateral con las letras
+        return animalSectionTitles
+    }
+    
+    //HACE QUE AL PINCHAR LA BARRA LATERAL TE LLEVE A LA SECCION CORRECTA
+    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        
+        guard let index = animalesTituloSeccion.index(of: title)//Mira en el array que teniamos y lo comprueba con los titulos. Si existe devuelve index y si no -1
+            else {
+            return -1
+        }
+        return index
+    }
     
     
+    //MODIFICA EL TAMAÑO DE LAS SECCIONES
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let headerView = view as! UITableViewHeaderFooterView
+        
+        
+        
+        //SI QUIERES PONER LA SECCION A TU GUSTO CREARIAS UNA CLASE COMO HACEMOS CON LAS CELDAS DE TIPO UIVIEW Y LA LLAMARIAMOS DONDE PONEMOS view as! UITABLEViewHeaderFooterView POR EJEMPLO view as! UITableViewMiCeldaCustomizada
+        
+        
+        
+        headerView.backgroundView?.backgroundColor = UIColor(red: 231.0/255.0, green: 240.0/255.0, blue: 241.0/255.0, alpha: 1.0)
+        headerView.textLabel?.font = UIFont(name: "Avenir", size: 27.0)
+    }
+    
+    
+    //RETORNA EL NUMERO DE FILAS POR SECCIONES
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
         
